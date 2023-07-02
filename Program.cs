18d6983a614a06ac1ee6ffe452c43fe650f2b5df
kundai <kundiemajome@gmail.com>
2023-07-02 21:18:15 +0200

    made changes

--- Program.cs
+++ Program.cs
@@ -4,5 +4,14 @@
     {
         Console.WriteLine("Label");
         int x = 67;
+        int y = 50;
+        wadyeyi("chingwa");
+    }
+
+    string wadyeyi(string sadza)
+    {
+        Console.WriteLine("Wadyeyi?");
+        Console.WriteLine(sadza);
+
     }
 }
